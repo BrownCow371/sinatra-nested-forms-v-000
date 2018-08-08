fb20a319189bf43726cd944ca7754fc926aa6394
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 21:10:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,15 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ship].each do |details|
+        Ship.new(details)
+      end
+
+      @ships=Ship.all
+
+      
       erb :"/pirates/show"
     end
   end
