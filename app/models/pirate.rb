835564ac5a3651e3e4af292bc806d4b2b22cc6dd
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 20:46:29 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:weight]
+    @height = arg[:height]
+    @@all<<self
+  end
+
 end
